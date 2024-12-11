4f030b7ab6eeded1c908dc8bf4346276fcda94f5
isabekk991 <i46305033@gmail.com>
2024-12-11 15:23:39 +0600

    Ваше сообщение коммита

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+API_TOKEN = "7916062297:AAEhmcyZU7A5WPj318g982Zavk1EJwfqKLs"
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
